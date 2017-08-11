17ce45113936ba27f418739e409b98398a2740b5
neliza <nicolasaelf64@gmail.com>
2017-08-11 10:02:57 -0600

    game working with minor flaws

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,64 @@
 //Global Variables
-var letters = ["a","b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var letters = ["a","b","c", "d", "e", "f", "g", "h",
+ "i", "j", "k", "l", "m", "n", "o",
+ "p", "q", "r", "s", "t", "u", "v",
+  "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
-var guessLeft = 9
-var guessedLetters = [] //array to store user input
+var maxGuesses = 9
+var guessedLettersArray = [] //array to store user input
 
 
+//Computer chooses a random letter
+var computerChoice  = letters[Math.floor(Math.random() * letters.length)];
+ 		console.log("this is the computer guess " + computerChoice);
 
-
+//user guesses the random letter
 document.onkeyup = function(event){
-	// var userguess = String.fromCharCode(event.keyCode).toLowerCase();
+	console.log(event);
 	
 	var userGuess = event.key; //variable to store userGuess
 		console.log(userGuess);
+	
+	//Eliminate user to lower case alphabet
+	function isLowerAlpha(event){
+	var keyCode = event.key
+	(keyCode < 65 || keyCode > 90)
+	}
 
-	guessedLetters.push(userGuess);
+	
+//user gets nine guesses
+	guessedLettersArray.push(userGuess)
+	console.log("This is the guessedLettersArray " + guessedLettersArray)
 
-	var computerChoice = computerChoice = letters[Math.floor(Math.random() * letters.length)];
- 		console.log(computerChoice);
 
- 			//Determine if user guess is equal to computers guess
+//if the user guesses the letter correctly then they win
+//if not, the user looses a guess
+//after loosing all nine guesses they loose the game
+//Determine if user guess is equal to computers guess
  			if (userGuess === computerChoice){
  				wins++
- 				guessLeft--
-
+ 				console.log("you win")
  			}
- 			 else{
- 				losses++
+ 			 else if(guessedLettersArray.length >= maxGuesses){
+ 				losses++ 
+ 				console.log("you loose")
  			}
+ 			else {
+ 				console.log("You didn't win or loose")
+ 			};
+ //TODO: Limit user guesses to 9.
 
- 			if (guessLeft === 0){
- 				losses++
- 				guessLeft--
- 			}
+ //Replace inner HTML
+ //document.getElementById(id).innerHTML = new HTML
 
+ document.getElementById("wins").innerHTML = "Wins: " + wins;
+ document.getElementById("losses").innerHTML = "Losses: " +losses;
+ document.getElementById("maxGuesses").innerHTML = "Guesses Left: " + maxGuesses;
+ document.getElementById("guessedLetters").innerHTML = "Your guesses so far: " + guessedLettersArray; 
 
- 			//Replace inner HTML
- 			var html= + "Wins: " + wins + "Loses: "+ losses + "Guesses Left: " + guessesLeft + "Guessed Letters: " +  guessedLetters.join(', '); //Show guessed letters here
-    		document.querySelector("#game").innerHTML = html;
-
-    	}
-
-
+ 			
+};
 
 
 
